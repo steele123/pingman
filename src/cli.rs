81c316e49a8d3096e157695e12dd6f6f2aa8b3df
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-06-17 13:29:05 -0400

    Added docs to subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -14,30 +14,44 @@ pub enum Subcommand {
     Proxy(Proxy),
 }
 
+/// various methods of pinging through http
 #[derive(Clap)]
 pub struct Ping {
+    /// the site that should get pinged
     #[clap(short, long, default_value = "https://google.com")]
     pub site: String,
+    /// the amount of requests that should be sent per site
     #[clap(short, default_value = "10")]
     pub pings: i32,
+    /// whether or not pingman should use get requests instead of head requests, get requests are much slower but could be a more valid test for you
     #[clap(short)]
     pub get: bool,
 }
 
+/// various methods of pinging with a proxy / proxies
 #[derive(Clap)]
 pub struct Proxy {
+    /// the site that should get pinged
     #[clap(short, long, default_value = "https://google.com")]
     pub site: String,
+    /// file with proxies in them
     #[clap(short)]
     pub file: Option<String>,
     // TODO
+    /// the format to parse proxies from the specified file
+    #[clap(required(false))]
     pub format: String,
     // TODO
+    /// a proxy formatted by default as ip:port ip:port:username:password
+    #[clap(required(false))]
     pub proxy: String,
+    /// the path to a location on your computer to save the test results in json format
     #[clap(short)]
     pub output: Option<String>,
+    /// whether or not pingman should use get requests instead of head requests, get requests are much slower but could be a more valid test for you
     #[clap(short)]
     pub get: bool,
+    /// amount of requests that each proxy should do
     #[clap(short, default_value = "10")]
     pub pings: i32,
 }
